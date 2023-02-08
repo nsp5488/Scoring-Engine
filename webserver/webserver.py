fc0e24d11f5789e65c46d779aa85b5b687573a71
nsp5488 <nsp5488@g.rit.edu>
2023-02-08 16:11:48 -0500

    Implemented a basic frontend

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,40 +1,37 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, request, redirect
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-
-services = {'8.8.8.8' : {'dns': {'service': 'dns', 'status': 'UP', 'host': '8.8.8.8'}},
-'http://google.com' : {'http': {'service': 'http', 'status': 'UP', 'host': 'http://google.com'}},
-'localhost': {'icmp' : {'service': 'icmp', 'status': 'UP', 'host': 'localhost'}, 'icmp': {'service': 'smtp', 'status': 'DOWN', 'host': 'localhost'}}}
-
-
+services = {}
 
 @app.route("/")
-def hello_world():
-    return "<p> test</p>"
+def landing_page():
+    return redirect('/display_scores')
 
 @app.route("/display_scores", methods=['GET'])
 def display_services():
-    g.services = services
-    g.len = len(services)
-    return render_template('index.html')
+    print(f"Received {request} from front-end")
+    return services
 
 
 @app.route("/update_scores", methods=['POST'])
 def update_services():
-    print('test')
-    service_info = request.form['service']
-    print(service_info)
-    host = service_info['host']
-    service = service_info['service']
-    status = service_info['status']
-    print(service_info + 'in flask')
+    try:
+        print(f"Received {request} FROM {request.host}")
 
-    if services.has_key(host):
-        if services[host].has_key(service):
-            services[host][service] = status
+        host = request.form['host']
+        service = request.form['service']
+        status = request.form['status']
+
+        if host in services.keys():
+            if service in services[host].keys():
+                services[host][service] = status
+            else:
+                services[host][service] = status
         else:
-            services[host][service] = service_info
-    else:
-        services[host] = {service : service_info}
-    
+            services[host] = {service : status}
+        return {'Success': True}
+    except Exception:
+        return {'Success' : False}
